61805e2e8013a7a72676e5bfabcdc891afd4006f
AdelYACINE <ahmed95yacine@gmail.com>
2024-01-23 15:44:12 +0100

    add new project

--- src/Helper/ProjectsList.js
+++ src/Helper/ProjectsList.js
@@ -5,17 +5,25 @@ import p4 from "../imgs/p4.jpg";
 import p5 from "../imgs/p5.png";
 import p6 from "../imgs/p6.png";
 import p7 from "../imgs/p7.jpg";
+import p10 from "../imgs/p10.jpg";
 
 export const ProjectsList = [
   {
     id: 1,
+    name: "E-commerce product page using react.js(context api/vite)",
+    pic: p10,
+    live: "https://product-page-yacine.netlify.app/",
+    repo: "https://github.com/AdelYACINE/react-e-commerce-product-page",
+  },
+  {
+    id: 2,
     name: "newsletter sign-up form with using react.js",
     pic: p1,
     live: "https://newsletter-yacine.netlify.app/",
     repo: "https://github.com/AdelYACINE/react-Newsletter-sign-up-form-with-success-message",
   },
   {
-    id: 2,
+    id: 3,
 
     name: "manage landing page project using react.js with vite",
     pic: p7,
@@ -23,35 +31,35 @@ export const ProjectsList = [
     repo: "https://github.com/AdelYACINE/react-manage-landing-page",
   },
   {
-    id: 3,
+    id: 4,
     name: "react-crypto-app",
     pic: p3,
     live: "https://last-update-crypto-adelmadrid-app.netlify.app/",
     repo: "https://github.com/AdelYACINE/react-crypto-app",
   },
   {
-    id: 4,
+    id: 5,
     name: "Space multi-page website using react.js(vite,react-router-dom)",
     pic: p4,
     live: "https://space-yacine-web.netlify.app/",
     repo: "https://github.com/AdelYACINE/react-space-website",
   },
   {
-    id: 5,
+    id: 6,
     name: "react-food-website",
     pic: p5,
     live: "https://food-adelmadrid.netlify.app/",
     repo: "https://github.com/AdelYACINE/react-food-website",
   },
   {
-    id: 6,
+    id: 7,
     name: "react-shopping-card",
     pic: p6,
     live: "https://commerce-adelmadrid.netlify.app/",
     repo: "https://github.com/AdelYACINE/react-shopping-card",
   },
   {
-    id: 7,
+    id: 8,
     name: "react-todolist-app",
     pic: p2,
     live: "https://last-update-adelmadrid-todolist.netlify.app/",
